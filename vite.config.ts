aea3ed6ddeb5716b76c8c550e23e440b89f9d827
cardosofiles <cardosofiles@outlook.com>
2025-02-24 17:28:37 -0300

    fix: fixing import error

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import * as path from 'node:path' // Alternativa para importação correta
 
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve('./src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
